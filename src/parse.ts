cb4629022335ab52d8143fcb7b87cf6491cf8516
ss49919201 <sakashin10291029@gmail.com>
2024-12-27 17:35:06 +0900

    refactor: define types

--- src/parse.ts
+++ src/parse.ts
@@ -1,26 +1,56 @@
-export function parse(s: string): unknown {
-  const tokens = tokenize(s);
-
-  let state;
-  let value = "";
-  for (const token of tokens) {
-    s: switch (token.type) {
-      case "quotation-mark":
-        if (state === "start-string") {
-          return value;
-        }
-
-        state = "start-string";
-        break s;
-      case "value":
-        value += token.value;
-        break s;
-    }
+function assert(b: boolean, msg: string) {
+  if (!b) {
+    throw new Error(msg);
   }
 }
 
+class UnexpectedFirstCharacter extends Error {
+  constructor(token: Token) {
+    super(`unexpected first character: ${token.value}`);
+  }
+}
+
+function parseString(tokens: Token[]): string {
+  assert(
+    tokens[0].type === "quotation-mark",
+    "First token must be quotation-mark."
+  );
+  assert(
+    tokens.slice(-1)[0].type === "quotation-mark",
+    "Last token must be quotation-mark."
+  );
+
+  return tokens
+    .slice(1, -1)
+    .map(({ value: char }) => char)
+    .join("");
+}
+
+function throwUnexpectedFirstTokenError(tokens: Token[]): never {
+  throw new UnexpectedFirstCharacter(tokens[0]);
+}
+
+type ParseFn = typeof parseString | typeof throwUnexpectedFirstTokenError;
+
+const firstTokenToParseFn = {
+  "quotation-mark": parseString,
+  value: throwUnexpectedFirstTokenError,
+} as const satisfies {
+  [k in TokenType]: ParseFn;
+};
+
+export function parse(s: string): unknown {
+  if (s.length === 0) return "";
+
+  const tokens = tokenize(s);
+  const parseFn = firstTokenToParseFn[tokens[0].type];
+  return parseFn(tokens);
+}
+
+type TokenType = "quotation-mark" | "value";
+
 type Token = {
-  type: "quotation-mark" | "value";
+  type: TokenType;
   value: string;
 };
 
